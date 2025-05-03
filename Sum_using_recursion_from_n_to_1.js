3cf245523530dd9ea9b93c5a48d4e29d4e8bf0a8
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-05-03 17:41:21 +0530

    added the solution for Sum_using_recursion_from_n_to_1

--- Sum_using_recursion_from_n_to_1.js
+++ Sum_using_recursion_from_n_to_1.js
@@ -1,1 +1,13 @@
-//test
+function sum(n){
+    if(n == 1) return 1;
+    return n + sum(n-1);
+}
+console.log(sum(10))
+
+// this is the forumula to calculate the sum of n numbers
+// 1 + 2 + 3 + ... + n = n * (n + 1) / 2
+// this is the iterative way to calculate the sum of n numbers
+function sum2(n){
+    return (n * ( n + 1))/2
+}
+console.log(sum(10))

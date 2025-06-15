b8a5e65a26fbc909988564f3e5d9c9ce2807e496
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-06-15 18:14:28 +0530

    added the GCD

--- GCD.js
+++ GCD.js
@@ -1,4 +1,5 @@
 //Euclidean Algorithm
+//optimize Approach
 function findGCD(n1,n2){
     const smaller = n1 > n2 ? n2 : n1 ;
     const greater =  n1 > n2 ? n1 : n2 ;
@@ -9,3 +10,16 @@ function findGCD(n1,n2){
     return findGCD(sub, smaller)
 }
 console.log(findGCD(20,15))
+
+//Better Approach
+// function findGCD(n1,n2){
+//     const smaller = n1 > n2 ? n2 : n1 ;
+//     let GCD=1;
+//     for(let i = 1; i <= smaller; i++){
+//         if( n1%i==0 && n2%i==0){
+//             GCD = i;
+//         }
+//     }
+//     return GCD;
+// }
+// console.log(findGCD(20,40))
